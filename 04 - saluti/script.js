6968b535d7fd8fd8283a3d25dc0d3b4d51d678fd
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-21 16:27:19 +0200

    es 4

--- 04 - saluti/script.js
+++ 04 - saluti/script.js
@@ -1,13 +1,17 @@
 /* Scrivi una funzione che accetti una stringa contenente un nome e restituisca un saluto seguito dal nome fornito (es. "Ciao [nome]") */
 
-const name = 'Mario';
-
+const name = "Mario";
 
 // Dichiara la funzione qui.
+function sayHello(name) {
+  return "Ciao " + name;
+}
 
+// * ARROW FUNCTION
+arrowFunction = (name) => "Ciao " + name;
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(sayHello("Mario"));
+console.log(arrowFunction("Mario"));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
