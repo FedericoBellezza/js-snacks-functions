74bc46ae8fc058b12ddd305395467b9eae0da431
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-10-21 16:23:32 +0200

    es 3

--- 03 - conta vocali/script.js
+++ 03 - conta vocali/script.js
@@ -1,13 +1,34 @@
 /* Scrivi una funzione che accetti una stringa e restituisca il numero di vocali contenute al suo interno */
-
-const word = 'javascript';
-
+const word = "javascript";
 
 // Dichiara la funzione qui.
+function howManyVowels(string) {
+  let resultArray = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
 
+  for (i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      resultArray++;
+    }
+  }
+  return resultArray;
+}
+
+// * ARROW FUNCTION
+arrowFunction = (string) => {
+  let resultArray = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+
+  for (i = 0; i < string.length; i++) {
+    if (vowels.includes(string[i])) {
+      resultArray++;
+    }
+  }
+  return resultArray;
+};
 
 // Invoca la funzione qui e stampa il risultato in console
-
-
+console.log(howManyVowels("javascript"));
+console.log(arrowFunction("javascript"));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
